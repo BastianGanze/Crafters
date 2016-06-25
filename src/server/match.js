6d072045df7c20a345815cd5f77ca7270f6338d7
Bastian Ganze <bastian.ganze@developers.imitches.de>
2016-06-25 23:15:29 +0200

    * add push

---
+++ src/server/match.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const ConnManager = require("./conn_manager");
+const MapManager = require("./map_manager");
+const World = require("./world");
+const PlayerManager = require("./player_manager");
+const Vector2D = require("./utils/vector");
+
+class Game {
+
+    constructor(io) {
+        this.team1Resources = {};
+        this.team2Resources = {};
+        this.neededResources = {};
+    }
+    
+    setupMatch()
+    {
+        this.neededResources = {"a":3,"b":2,"c":1};
+        this.team1Resources = {};
+        this.team2Resources = {};
+        for(var resource in this.neededResources)
+        {
+            this.team1Resources[resource] = 0;
+            this.team2Resources[resource] = 0;
+        }
+    }
+    
+    
+
+
+}
+
+module.exports = Game;
