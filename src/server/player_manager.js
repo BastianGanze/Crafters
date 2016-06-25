040d2df48273a84ca17cdde74a2a472856ba3c73
Bastian Ganze <bastian.ganze@developers.imitches.de>
2016-06-25 13:02:16 +0200

    * added player manager and world

--- src/server/player_manager.js
+++ src/server/player_manager.js
@@ -1,1 +1,47 @@
 "use strict";
+
+class Player{
+
+    constructor(id, name, collisionObject)
+    {
+        this.id = id;
+        this.name = name;
+        this.collisionObject = collisionObject;
+    }
+}
+
+class PlayerManager{
+
+    constructor(world)
+    {
+        this.players = new Map();
+        this.world = world;
+        this.playerCount = 0;
+    }
+
+    getPlayerAsJson(player)
+    {
+        if(player.collisionObject && typeof player.collisionObject.toJSON == "function")
+        {
+            return JSON.stringify({"id":player.id, "name":player.name, "physProps":player.collisionObject.toJSON()});
+        }
+        else
+        {
+            console.log("Could not get collision information from player, collisionObject was of Type \""+typeof player.collisionObject+"\"");
+            return null;
+        }
+    }
+
+    getUniqueId()
+    {
+        this.playerCount++;
+        return this.playerCount;
+    }
+
+    createPlayer(name)
+    {
+        var uId = this.getUniqueId();
+        this.players.set(uId, new Player(uId, name));
+    }
+
+}
--- src/server/world.js
+++ src/server/world.js
@@ -1,1 +1,68 @@
 "use strict";
+
+class PlayerCollider{
+
+    constructor(id, position, radius)
+    {
+        this.id = id;
+        this.type = "CIRCLE";
+        this.position = position;
+        this.velocity = {x:0,y:0};
+        this.radius = radius;
+    }
+}
+
+class World{
+    
+    constructor()
+    {
+        this.colliderIds = 0;
+        this.collisionObjects = {};
+        this.collisionCallbacks = [];
+    }
+
+    getUniqueId()
+    {
+        this.colliderIds++;
+        return this.colliderIds;
+    }
+    
+    createPlayerCollider(position, radius)
+    {
+        var playerCollider = new PlayerCollider(this.getUniqueId(), position, radius);
+        return playerCollider;
+    }
+    
+    addToCollision(collisionObject)
+    {
+        this.collisionObjects[collisionObject.id] = collisionObject;
+    }
+    
+    removeFromCollision(collisionObject)
+    {
+        delete this.collisionObjects[collisionObject.id];
+    }
+    
+    updatePhysicStep(delta)
+    {
+        //TODO: Update physic for each object and do collision
+    }
+    
+    collisionHappened(collider_1, collider_2, relativeVelocity)
+    {
+        for(var i=0; i < this.collisionCallbacks; i++)
+        {
+            this.collisionCallbacks[i](collider_1, collider_2, relativeVelocity);         
+        }
+    }
+    
+    update(delta)
+    {
+        this.updatePhysicStep(delta);    
+    }
+    
+    addCollisionCallback(callback)
+    {
+        this.collisionCallbacks.push(callback);
+    }
+}
